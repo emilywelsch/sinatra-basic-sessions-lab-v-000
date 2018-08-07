0611e049ffdc4ad2c5062318643967ddb9c0e5f0
Emily Welsch <emwelsch@gmail.com>
2018-08-07 13:09:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
   get '/' do
+    session["name"] = "Victoria"
+    @session = session
     erb :index
   end
 
