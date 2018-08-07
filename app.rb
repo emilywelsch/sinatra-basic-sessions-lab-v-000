e859386350a287a0c8e8356c0b88e500027ba53a
Emily Welsch <emwelsch@gmail.com>
2018-08-07 13:02:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    # displays the shopping cart contents
+    # sets the params in the session hash
+  end
 end
